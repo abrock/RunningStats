9707a82d5645eaf116598ba287980779d826bf60
Alexander <Brock.Alexander@web.de>
2020-08-31 13:15:10 +0200

    Add test with 10M points for Stats2D

--- test/test-2d.cpp
+++ test/test-2d.cpp
@@ -56,6 +56,25 @@ TEST(Plot2D, Stats2Dfixed) {
     stats.plotHist("stats-fixed-normal-2d-ignore-0.001", bin, conf);
 }
 
+TEST(Plot2D, Stats2Dfixed_large) {
+    std::normal_distribution<double> dist;
+
+    runningstats::Stats2D<float> stats;
+
+    for (size_t ii = 0; ii < 10*1000*1000; ++ii) {
+        stats.push_unsafe(.1*dist(engine), dist(engine));
+    }
+
+    std::pair<double, double> bin = stats.FreedmanDiaconisBinSize();
+    HistConfig conf;
+    conf.setTitle("2D normal distribution, F-D bin width").setXLabel("N(0,0.1²)").setYLabel("N(0,1²)").setLogCB();
+    stats.plotHist("stats-fixed-normal-2d-large", bin, conf);
+
+    conf.setIgnoreAmount(.001);
+    conf.setTitle("2D normal distribution, F-D bin width").setXLabel("N(0,0.1²)").setYLabel("N(0,1²)").setLogCB();
+    stats.plotHist("stats-fixed-normal-2d-large-ignore-0.001", bin, conf);
+}
+
 TEST(Plot2D, Stats2Dfixed_degenerate) {
     std::normal_distribution<double> dist;
 
